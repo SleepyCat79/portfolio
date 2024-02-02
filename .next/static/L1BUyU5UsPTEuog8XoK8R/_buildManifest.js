e05dce36e4cba6eedb72535ee77edb4bf7e8f932
Sleepy <60354077+SleepyCat79@users.noreply.github.com>
2024-02-02 18:34:15 +0700

    publish

---
+++ .next/static/L1BUyU5UsPTEuog8XoK8R/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/825-f54b5cc851307338.js","static/chunks/pages/index-bdf013e1a99b4c26.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
